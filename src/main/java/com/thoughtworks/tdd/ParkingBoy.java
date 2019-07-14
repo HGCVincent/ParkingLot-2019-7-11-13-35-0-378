5f45a70199688eca320df82fe85c3f4e45338ee4
Vincent <1061271330@qq.com>
2019-07-14 19:07:05 +0800

    [HGC:]return ticket when petch car given first parking lot is full

--- src/main/java/com/thoughtworks/tdd/ParkingBoy.java
+++ src/main/java/com/thoughtworks/tdd/ParkingBoy.java
@@ -4,30 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingBoy {
-    private ParkingLot parkingLot;
+    private List<ParkingLot> parkingLots = new ArrayList<>();
 
     public ParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
+        this.parkingLots.add(parkingLot);
     }
 
-    public ParkingTicket parkCar(Car car) {
-        if (car == null){
+    public ParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+    }
+
+    public void addParkingLots(ParkingLot parkingLots) {
+        this.parkingLots.add(parkingLots);
+    }
+
+    public ParkingTicket parkCar(Car car, Customer customer) {
+        ParkingTicket ticket = null;
+        if (car == null) {
             return null;
         }
-        return parkingLot.parkCar(car);
+        for (ParkingLot parkingLot : parkingLots) {
+            ticket = parkingLot.parkCar(car);
+            if (ticket != null){
+                return ticket;
+            }
+            if (ticket == null && parkingLot == parkingLots.get(parkingLots.size() - 1)) {
+                customer.setErrorMessage("Not enough position.");
+            }
+        }
+        return ticket;
     }
 
     public Car fetchCar(ParkingTicket ticket,Customer customer) {
+        Car car = null;
         if (ticket == null){
             customer.setErrorMessage("Please provide your parking ticket.");
             return null;
         }
-        else {
-            Car car = parkingLot.fetchCar(ticket);
+        for (ParkingLot parkingLot : parkingLots){
+            car = parkingLot.fetchCar(ticket);
             if (car == null){
                 customer.setErrorMessage("Unrecognized parking ticket");
             }
-            return car;
         }
+        return car;
     }
 }
--- src/test/java/com/thoughtworks/tdd/ParkingLotTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingLotTest.java
@@ -15,7 +15,7 @@ public class ParkingLotTest {
         ParkingLot parkingLot = new ParkingLot(1);
         Car car = new Car();
         ParkingBoy parkingBoy = new ParkingBoy (parkingLot);
-        ParkingTicket ticket = parkingBoy.parkCar(car);
+        ParkingTicket ticket = parkingBoy.parkCar(car,customer);
 
         //When
         Car fetchCar = parkingBoy.fetchCar(ticket,customer);
@@ -33,7 +33,7 @@ public class ParkingLotTest {
         Car car = new Car();
         Customer customer = new Customer();
         ParkingBoy parkingBoy = new ParkingBoy (parkingLot);
-        ParkingTicket corretTicket = parkingBoy.parkCar(car);
+        ParkingTicket corretTicket = parkingBoy.parkCar(car,customer);
         ParkingTicket wrongTicket = new ParkingTicket(null);
 
         //When
@@ -52,7 +52,7 @@ public class ParkingLotTest {
         ParkingLot parkingLot = new ParkingLot(1);
         Car car = new Car();
         ParkingBoy parkingBoy = new ParkingBoy (parkingLot);
-        ParkingTicket corretTicket = parkingBoy.parkCar(car);
+        ParkingTicket corretTicket = parkingBoy.parkCar(car,customer);
 
         //When
         Car fetchCar = parkingBoy.fetchCar(null,customer);
@@ -70,7 +70,7 @@ public class ParkingLotTest {
         ParkingLot parkingLot = new ParkingLot(1);
         Car car = new Car();
         ParkingBoy parkingBoy = new ParkingBoy (parkingLot);
-        ParkingTicket ticket = parkingBoy.parkCar(car);
+        ParkingTicket ticket = parkingBoy.parkCar(car,customer);
 
         //When
         Car fetchCar1 = parkingBoy.fetchCar(ticket,customer);
@@ -88,13 +88,15 @@ public class ParkingLotTest {
         ParkingLot parkingLot = new ParkingLot(2);
         ParkingBoy parkingBoy = new ParkingBoy (parkingLot);
         for (int i = 0; i < 2; i++){
+            Customer customer = new Customer();
             Car car = new Car();
-            parkingBoy.parkCar(car);
+            parkingBoy.parkCar(car,customer);
         }
+        Customer customer3 =new Customer();
         Car car3 = new Car();
 
         //When
-        ParkingTicket ticket = parkingBoy.parkCar(car3);
+        ParkingTicket ticket = parkingBoy.parkCar(car3,customer3);
 
         // Then
         Assertions.assertSame(null, ticket);
@@ -106,9 +108,10 @@ public class ParkingLotTest {
         //Given
         ParkingLot parkingLot = new ParkingLot(1);
         ParkingBoy parkingBoy = new ParkingBoy (parkingLot);
+        Customer customer = new Customer();
 
         //When
-        ParkingTicket ticket = parkingBoy.parkCar(null);
+        ParkingTicket ticket = parkingBoy.parkCar(null,customer);
 
         // Then
         Assertions.assertSame(null, ticket);
@@ -122,7 +125,7 @@ public class ParkingLotTest {
         ParkingBoy parkingBoy = new ParkingBoy (parkingLot);
         Customer customer = new Customer();
         Car car = new Car();
-        ParkingTicket ticket = parkingBoy.parkCar(car);
+        ParkingTicket ticket = parkingBoy.parkCar(car,customer);
         parkingBoy.fetchCar(ticket,customer);
 
 
@@ -152,4 +155,47 @@ public class ParkingLotTest {
         Assertions.assertEquals("Please provide your parking ticket.",Mes);
     }
 
+    @Test
+    public void should_return_message_about_not_enough_position_when_query_error_massage_given_capacity_is_full () {
+
+        //Given
+        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingBoy parkingBoy = new ParkingBoy (parkingLot);
+        Customer customer1 = new Customer();
+        Car car1 = new Car();
+        parkingBoy.parkCar(car1,customer1);
+        Customer customer2 =new Customer();
+        Car car2 = new Car();
+
+        //When
+        parkingBoy.parkCar(car2,customer2);
+        String Mes = customer2.queryErrorMessage();
+
+        // Then
+        Assertions.assertEquals("Not enough position.",Mes);
+    }
+
+    @Test
+    public void should_return_ticket_when_petch_car_given_first_parking_lot_is_full() {
+
+        //Given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        for (int i = 0; i < 2; i++){
+            ParkingLot parkingLot = new ParkingLot(1);
+            parkingLots.add(parkingLot);
+        }
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Customer customer1 = new Customer();
+        parkingBoy.parkCar(car1,customer1);
+        Car car2 = new Car();
+        Customer customer2 = new Customer();
+
+        //When
+        ParkingTicket parkingTicket = parkingBoy.parkCar(car2,customer2);
+
+        // Then
+        Assertions.assertNotNull(parkingTicket);
+
+    }
 }
