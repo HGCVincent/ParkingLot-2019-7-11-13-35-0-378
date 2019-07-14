ed88d63cb1c0fff406b3644a0759f9a0e39d2f27
Vincent <1061271330@qq.com>
2019-07-14 15:34:30 +0800

    return no ticket when parking car given more then limit cars

--- src/main/java/com/thoughtworks/tdd/ParkingLot.java
+++ src/main/java/com/thoughtworks/tdd/ParkingLot.java
@@ -6,24 +6,45 @@ import java.util.List;
 public class ParkingLot {
     private List<Car> cars = new ArrayList<>();
     private List<ParkingTicket> tickets = new ArrayList<>();
+    private int capacity;
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
 
     public ParkingTicket parkCar(Car car){
-        cars.add(car);
-        ParkingTicket ticket = new ParkingTicket(car);
-        tickets.add(ticket);
-        return ticket;
+        if (isFull()){
+            return null;
+        }
+        else {
+            cars.add(car);
+            ParkingTicket ticket = new ParkingTicket(car);
+            tickets.add(ticket);
+            return ticket;
+        }
     }
 
     public Car fetchCar(ParkingTicket ticket){
         if (cars.contains(ticket.getCar())) {
             tickets.remove(ticket);
             cars.remove(ticket.getCar());
+            capacity--;
             return ticket.getCar();
         }
         else{
             return null;
         }
     }
+
+    public boolean isFull(){
+        if (cars.size() >= capacity){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
 
 
 
--- src/test/java/com/thoughtworks/tdd/ParkingLotTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingLotTest.java
@@ -11,7 +11,7 @@ public class ParkingLotTest {
     public void  should_return_car_when_fetch_car_given_have_ticket_by_parking_the_car () {
 
         //Given
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(1);
         Car car = new Car();
         ParkingBoy parkingBoy = new ParkingBoy (parkingLot);
         ParkingTicket ticket = parkingBoy.parkCar(car);
@@ -74,5 +74,24 @@ public class ParkingLotTest {
         // Then
         Assertions.assertSame(null, fetchCar2);
 
+    }
+
+    @Test
+    public void  should_return_no_ticket_when_fetch_car_given_more_then_limit_cars () {
+
+        //Given
+        ParkingLot parkingLot = new ParkingLot(2);
+        ParkingBoy parkingBoy = new ParkingBoy (parkingLot);
+        for (int i = 0; i < 2; i++){
+            Car car = new Car();
+            parkingBoy.parkCar(car);
+        }
+        Car car3 = new Car();
+
+        //When
+        ParkingTicket ticket = parkingBoy.parkCar(car3);
+
+        // Then
+        Assertions.assertSame(null, ticket);
     }
 }
